737a6163e472bb2c165490618e2000b228e35b16
Beno Suriano <bjsuriano@gmail.com>
2021-12-12 17:03:28 -0400

    Spray score-based confetti onto the screen.

--- ResultsViewController.swift
+++ ResultsViewController.swift
@@ -7,21 +7,67 @@
 //
 
 import UIKit
+import ConfettiView
 
 class ResultsViewController: UIViewController {
 
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var slyCommentLabel: UILabel!
-
+    @IBOutlet weak var confetti: ConfettiView!
+    
     var score: String?
     var slyComment: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
         scoreLabel.text = score ?? "Oops... "
         slyCommentLabel.text = slyComment ?? "You win, we lose!"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let quizScoreDouble = Double(score!) {
+            switch quizScoreDouble {
+            case 0.0:
+                confetti.emit(with: [
+                    .text("🤐"),
+                    .text("🤭"),
+                    .shape(.circle, .darkGray),
+                    .shape(.square, .black)
+                ])
+            case 0.1..<49.9:
+                confetti.emit(with: [
+                    .text("🤦‍♂️"),
+                    .text("📚"),
+                    .shape(.circle, .darkGray),
+                    .shape(.square, .black)
+                ])
+            case 50.0..<74.9:
+                confetti.emit(with: [
+                    .text("🤡"),
+                    .text("😉"),
+                    .shape(.circle, .blue),
+                    .shape(.square, .yellow)
+
+                ])
+            case 74.9..<89.9:
+                confetti.emit(with: [
+                    .text("🤩"),
+                    .text("👍"),
+                    .shape(.circle, .purple),
+                    .shape(.square, .yellow)
+                ])
+            default:
+                confetti.emit(with: [
+                    .text("🤩"),
+                    .text("👍"),
+                    .text("💯"),
+                    .shape(.circle, .purple),
+                    .shape(.square, .yellow)
+                ])
+            }
+        }
     }
     
     @IBAction func retakeQuizButtonPressed(_ sender: UIButton) {
